5c5d157ff48800e2f87f07e5b99ac55590ed1adc
Ali Eldor <alieldor02@gmail.com>
2025-03-16 01:18:08 +0200

    initialize register component

---
+++ gallery-client/src/pages/Register.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import config from "../config/config";
+
+
+
+  return (
+    <div className="login-outer-container">
+      <div className="login-container">
+        <div className="login-area">
+          <h3>REGISTER TO Gallery</h3>
+          <form
+            id="register-form"
+            className="login-items"
+            
+          >
+            <label htmlFor="fullname">Name</label>
+            <input
+              type="text"
+              className="login"
+              name="fullname"
+              placeholder="Enter your name"
+              value={formData.fullname}
+              
+              required
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className="login"
+              name="email"
+              placeholder="Enter your email"
+              value={formData.email}
+              
+              required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="login"
+              name="password"
+              placeholder="Enter password"
+              value={formData.password}
+              
+              required
+            />
+            <input type="submit" className="login-btn" value="Register" />
+          </form>
+          <p className="reg">
+            Already have an account?
+            <Link className="a" to="/login">
+              Please Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
