aa6b80619eace1fb1116b4a11ac1e226d7c03209
Ali Eldor <alieldor02@gmail.com>
2025-03-16 00:46:24 +0200

    initialize login component

---
+++ gallery-client/src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import API_BASE_URL from "../config/config"
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  
+
+  return(
+  <div className='login-outer-container'>
+      <div className='login-container'>
+        <div className='login-area'>
+          <h3>LOGIN TO Gallery</h3>
+          <form id="login-form" className='login-items' onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email" 
+              className='login' 
+              name="email" 
+              placeholder='Enter your email' 
+              value={formData.email}
+              onChange={handleChange}
+              required 
+            />
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password" 
+              className='login' 
+              name="password" 
+              placeholder="Enter Your Password" 
+              value={formData.password}
+              required 
+            />
+            <input 
+              type="submit" 
+              className='login-btn' 
+              value="Login" 
+            />
+          </form>
+          <p className='reg'>
+            New to FAQ? <Link className='a' to="/register">Create an Account</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Login;
